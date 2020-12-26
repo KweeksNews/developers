60c27bdb81ca2a5a8eb44adde10e1c52cb56877a
Ahmad Rulim <mfalkia342@gmail.com>
2020-12-26 15:42:33 +0700

    Prettify
    Signed-off-by: Ahmad Rulim <mfalkia342@gmail.com>

--- README.md
+++ README.md
@@ -3,23 +3,25 @@
 ### Parameter JSON
 
 #### assets/json/tech.json
-| Parameter | Deskripsi | Wajib |
-|--|--|--|
-| name | Nama teknologi | Ya |
-| image | Logo teknologi | Ya |
 
-* Logo teknologi harus berukuran `1024 x 1024 pixel` dengan format `svg`.
+| Parameter | Deskripsi      | Wajib |
+| --------- | -------------- | ----- |
+| name      | Nama teknologi | Ya    |
+| image     | Logo teknologi | Ya    |
+
+- Logo teknologi harus berukuran `1024 x 1024 pixel` dengan format `svg`.
 
 #### assets/json/webmaster.json
-| Parameter | Deskripsi | Wajib |
-|--|--|--|
-| name | Nama (atau nama alias) | Ya |
-| start | Waktu mulai bertugas | Ya |
-| finish | Waktu selesai bertugas | Tidak |
-| link | Tautan ke halaman web portofolio | Ya |
-| email | Alamat email | Ya |
-| github | Nama pengguna GitHub | Ya |
-| telegram | Nama pengguna Telegram | Ya |
 
-* Apabila nama tersusun lebih dari dua kata, maka tampilkan dua kata saja dan sisanya disingkat.
-* Parameter `finish` diisi apabila telah selesai bertugas. Selain itu, dapat dikosongkan saja.
+| Parameter | Deskripsi                        | Wajib |
+| --------- | -------------------------------- | ----- |
+| name      | Nama (atau nama alias)           | Ya    |
+| start     | Waktu mulai bertugas             | Ya    |
+| finish    | Waktu selesai bertugas           | Tidak |
+| link      | Tautan ke halaman web portofolio | Ya    |
+| email     | Alamat email                     | Ya    |
+| github    | Nama pengguna GitHub             | Ya    |
+| telegram  | Nama pengguna Telegram           | Ya    |
+
+- Apabila nama tersusun lebih dari dua kata, maka tampilkan dua kata saja dan sisanya disingkat.
+- Parameter `finish` diisi apabila telah selesai bertugas. Selain itu, dapat dikosongkan saja.
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,70 +1,56 @@
 $(document).ready(function () {
-  $("button.floatmenu").click(function () {
-    return $(".menu__wrapper").toggleClass("is__open"), !1;
+  $('button.floatmenu').click(function () {
+    return $('.menu__wrapper').toggleClass('is__open'), !1;
   });
   $(window).resize(function () {
-    $("#nav_menu").hasClass("is__open") &&
-      !$("button[class='floatmenu']").is(":visible") &&
-      $("#nav_menu").removeClass("is__open");
+    $('#nav_menu').hasClass('is__open') &&
+      !$("button[class='floatmenu']").is(':visible') &&
+      $('#nav_menu').removeClass('is__open');
   });
-  $.getJSON("/assets/json/tech.json", function (data) {
-    var tech = "";
-    for (var i = 0; i < data.length; i++) {
+  $.getJSON('/assets/json/tech.json', function (datas) {
+    let tech = '';
+    for (const data of datas) {
       tech +=
-        '<div class="box card"><img src="' +
-        data[i].image +
-        '" alt="' +
-        data[i].name +
-        '" title="' +
-        data[i].name +
-        '"><p>' +
-        data[i].name +
-        "</p></div>";
+        `<div class="box card"><img src="${data.image}" alt="${data.name}" ` +
+        `title="${data.name}"><p>${data.name}</p></div>`;
     }
-    $(".tech-list").append(tech);
+    $('.tech-list').append(tech);
   });
-  $.getJSON("/assets/json/webmaster.json", function (data) {
-    var profile = "",
-      indicator = "";
-    for (var i = 0; i < data.length; i++) {
+  $.getJSON('/assets/json/webmaster.json', function (datas) {
+    let profile = '';
+    let indicator = '';
+    for (const data of datas) {
       profile += '<div class="carousel-item"><div class="profile">';
-      if (data[i].ltw) {
+      if (data.ltw) {
         profile += '<span class="ltw">LTW</span>';
       }
       profile +=
-        '<img src="https://github.com/' +
-        data[i].github +
-        '.png?size=400" alt="' +
-        data[i].name +
-        '" title="' +
-        data[i].name +
-        '"><h3>' +
-        data[i].name +
-        "</h3><p>" +
-        data[i].start +
-        " - ";
-      if (data[i].finish) {
-        profile += data[i].finish;
+        `<img src="https://github.com/${data.github}.png?size=400"` +
+        `alt="${data.name}" title="${data.name}">` +
+        `<h3>${data.name}</h3><p>${data.start} - `;
+      if (data.finish) {
+        profile += data.finish;
       } else {
-        profile += "sekarang";
+        profile += 'sekarang';
       }
       profile +=
-        '</p><ul class="contact"><li><a href="https://' +
-        data[i].link +
-        '/" target="_blank" rel="noopener" aria-label="Site"><i class="icon-web"></i></a></li><li><a href="mailto:' +
-        data[i].email +
-        '" target="_blank" rel="noopener" aria-label="Email"><i class="icon-email"></i></a></li><li><a href="https://github.com/' +
-        data[i].github +
-        '/" target="_blank" rel="noopener" aria-label="GitHub"><i class="icon-github"></i></a></li><li><a href="https://t.me/' +
-        data[i].telegram +
-        '/" target="_blank" rel="noopener" aria-label="Telegram"><i class="icon-telegram"></i></a></li></ul></div></div>';
+        `</p><ul class="contact"><li><a href="https://${data.link}/" ` +
+        'target="_blank" rel="noopener" aria-label="Site"><i class="icon-web">' +
+        `</i></a></li><li><a href="mailto:${data.email}" target="_blank" ` +
+        'rel="noopener" aria-label="Email"><i class="icon-email"></i></a></li>' +
+        `<li><a href="https://github.com/${data.github}/" target="_blank" ` +
+        'rel="noopener" aria-label="GitHub"><i class="icon-github"></i></a>' +
+        `</li><li><a href="https://t.me/${data.telegram}/" target="_blank" ` +
+        'rel="noopener" aria-label="Telegram"><i class="icon-telegram"></i>' +
+        '</a></li></ul></div></div>';
       indicator +=
-        '<li data-target="#carousel-webmaster" data-slide-to="' + i + '"></li>';
+        '<li data-target="#carousel-webmaster" ' +
+        `data-slide-to="${datas.indexOf(data)}"></li>`;
     }
-    $("#items").append(profile);
-    $("#indicators").append(indicator);
-    $(".carousel-item").first().addClass("active");
-    $(".carousel-indicators > li").first().addClass("active");
+    $('#carousel-webmaster #items').append(profile);
+    $('#carousel-webmaster #indicators').append(indicator);
+    $('.carousel-item').first().addClass('active');
+    $('.carousel-indicators > li').first().addClass('active');
   });
-  $("#year").text(new Date().getFullYear());
+  $('#year').text(new Date().getFullYear());
 });
