6b71614a18d423eedafb216397d5c08426c707e8
Ahmad Rulim <mfalkia342@gmail.com>
2020-12-26 12:34:14 +0700

    Add editor and illustrator
    Also prettify some style
    
    Signed-off-by: Ahmad Rulim <mfalkia342@gmail.com>

--- assets/js/script.js
+++ assets/js/script.js
@@ -29,12 +29,12 @@ $(document).ready(function () {
         `alt="${data.name}" title="${data.name}">` +
         `<h3>${data.name}</h3><p>${data.start} - `;
       if (data.finish) {
-        profile += data.finish;
+        profile += `${data.finish}</p>`;
       } else {
-        profile += 'sekarang';
+        profile += 'sekarang</p>';
       }
       profile +=
-        `</p><ul class="contact"><li><a href="https://${data.link}/" ` +
+        `<ul class="contact"><li><a href="https://${data.link}/" ` +
         'target="_blank" rel="noopener" aria-label="Site"><i class="icon-web">' +
         `</i></a></li><li><a href="mailto:${data.email}" target="_blank" ` +
         'rel="noopener" aria-label="Email"><i class="icon-email"></i></a></li>' +
@@ -47,10 +47,62 @@ $(document).ready(function () {
         '<li data-target="#carousel-webmaster" ' +
         `data-slide-to="${datas.indexOf(data)}"></li>`;
     }
-    $('#carousel-webmaster #items').append(profile);
-    $('#carousel-webmaster #indicators').append(indicator);
-    $('.carousel-item').first().addClass('active');
-    $('.carousel-indicators > li').first().addClass('active');
+    $('#webmaster #items').append(profile);
+    $('#webmaster #indicators').append(indicator);
+    $('#webmaster .carousel-item').first().addClass('active');
+    $('#webmaster .carousel-indicators > li').first().addClass('active');
+  });
+  $.getJSON('/assets/json/editor.json', function (datas) {
+    let profile = '';
+    let indicator = '';
+    for (const data of datas) {
+      profile +=
+        '<div class="carousel-item"><div class="profile">' +
+        `<h3>${data.head}</h3>`;
+      for (const member of data.member) {
+        profile += `<h4>${member}</h4>`;
+      }
+      profile += `<p>${data.start} - `;
+      if (data.finish) {
+        profile += `${data.finish}</p>`;
+      } else {
+        profile += 'sekarang</p>';
+      }
+      profile += '</div></div>';
+      indicator +=
+        '<li data-target="#carousel-editor" ' +
+        `data-slide-to="${datas.indexOf(data)}"></li>`;
+    }
+    $('#editor #items').append(profile);
+    $('#editor #indicators').append(indicator);
+    $('#editor .carousel-item').first().addClass('active');
+    $('#editor .carousel-indicators > li').first().addClass('active');
+  });
+  $.getJSON('/assets/json/illustrator.json', function (datas) {
+    let profile = '';
+    let indicator = '';
+    for (const data of datas) {
+      profile +=
+        '<div class="carousel-item"><div class="profile">' +
+        `<h3>${data.head}</h3>`;
+      for (const member of data.member) {
+        profile += `<h4>${member}</h4>`;
+      }
+      profile += `<p>${data.start} - `;
+      if (data.finish) {
+        profile += `${data.finish}</p>`;
+      } else {
+        profile += 'sekarang</p>';
+      }
+      profile += '</div></div>';
+      indicator +=
+        '<li data-target="#carousel-illustrator" ' +
+        `data-slide-to="${datas.indexOf(data)}"></li>`;
+    }
+    $('#illustrator #items').append(profile);
+    $('#illustrator #indicators').append(indicator);
+    $('#illustrator .carousel-item').first().addClass('active');
+    $('#illustrator .carousel-indicators > li').first().addClass('active');
   });
   $('#year').text(new Date().getFullYear());
 });
